16473f5dfbf4ccdb18fa5185b6b039092363bf70
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-09-30 22:06:51 +0700

    feat: added htaccess config & init for app

---
+++ src/app/init.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'config/Config.php';
+
+require_once 'core/App.php';
+require_once 'core/Controller.php';
+
+require_once 'exceptions/LoggedException.php';
+
+require_once 'interfaces/ControllerInterface.php';
+require_once 'interfaces/ViewInterface.php';
