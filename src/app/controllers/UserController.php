addcccc5e02ab3208fffcb42a64b9de6c927d6b2
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-01 17:07:45 +0700

    feat: added UserController

---
+++ src/app/controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+class UserController extends Controller implements ControllerInterface
+{
+    public function index()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $loginView = $this->view('user', 'LoginView', []);
+                    $loginView->render();
+                    exit;
+
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+
+    public function login()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $loginView = $this->view('user', 'LoginView', []);
+                    $loginView->render();
+                    exit;
+
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+
+    public function register()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $registerView = $this->view('user', 'RegisterView', []);
+                    $registerView->render();
+                    exit;
+
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
+    }
+}
