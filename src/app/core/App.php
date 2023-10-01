25886e349aafe19a541f581ae2cf32d966c9fe6e
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-01 17:09:14 +0700

    refactor: changed to adapt to the existing code

--- src/app/core/App.php
+++ src/app/core/App.php
@@ -8,11 +8,43 @@ class App
 
     public function __construct()
     {
-        require_once __DIR__ . '/../controllers/HomeController.php';
-        $this->controller = new HomeController();
+        // require_once __DIR__ . '/../controllers/HomeController.php';
+        // $this->controller = new HomeController();
         $this->method = 'index';
-        $this->params = [];
+
+        $url = $this->parseURL();
+
+        $controllerPart = $url[0] ?? null;
+        if (isset($controllerPart) && file_exists(__DIR__ . '/../controllers/' . $controllerPart . 'Controller.php')) {
+            require_once __DIR__ . '/../controllers/' . $controllerPart . 'Controller.php';
+            $controllerClass = $controllerPart . 'Controller';
+            $this->controller = new $controllerClass();
+        }
+        unset($url[0]);
+
+        $methodPart = $url[1] ?? null;
+        if (isset($methodPart) && method_exists($this->controller, $methodPart)) {
+            $this->method = $methodPart;
+        }
+        unset($url[1]);
+
+        if (!empty($url)) {
+            $this->params = array_values($url);
+        } else {
+            $this->params = [];
+        }
 
         call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    private function parseURL()
+    {
+        if (isset($_SERVER['PATH_INFO'])) {
+            $url = trim($_SERVER['PATH_INFO'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
