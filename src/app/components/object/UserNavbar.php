6e8dd43b653d35eb57d257e478006398418cb3ad
Antonio Nattthan Krishna <92136335+natthankrish@users.noreply.github.com>
2023-10-02 12:49:40 +0700

    fix: animation transition

--- src/app/components/object/UserNavbar.php
+++ src/app/components/object/UserNavbar.php
@@ -1,15 +1,15 @@
 <navbar class="navbar-container">
     <div class="navbar-container">
         <div class="navbar">
-            <a href="photos.php" class="navbar-item">
+            <a href="/public/user/photos" class="navbar-item">
                 <img src="<?= BASE_URL ?>/assets/icons/photos.png" alt="My Photos">
                 <p class="navbar-item-desc">Photos</p>
             </a>
-            <a href="search.php" class="navbar-item">
+            <a href="/public/user/search" class="navbar-item">
                 <img src="<?= BASE_URL ?>/assets/icons/search.png" alt="Search">
                 <p class="navbar-item-desc">Search</p>
             </a>
-            <a href="feeds.php" class="navbar-item">
+            <a href="/public/user/feeds" class="navbar-item">
                 <img src="<?= BASE_URL ?>/assets/icons/feeds.png" alt="Feeds">
                 <p class="navbar-item-desc">Feeds</p>
             </a>
