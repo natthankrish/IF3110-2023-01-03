a0d7359717df258555743f60f300e49f9e10fb07
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-02 15:54:50 +0700

    feat: added database & tables functions

--- src/app/config/Config.php
+++ src/app/config/Config.php
@@ -2,3 +2,10 @@
 
 // URL
 define('BASE_URL', 'http://localhost:8080/public');
+
+// Database
+define('HOST', $_ENV['MYSQL_HOST']);
+define('DBNAME', $_ENV['MYSQL_DATABASE']);
+define('USER', $_ENV['MYSQL_USER'] ?? 'root');
+define('PASSWORD', $_ENV['MYSQL_PASSWORD']);
+define('PORT', $_ENV['MYSQL_PORT']);
---
+++ src/app/core/Database.php
@@ -0,0 +1,113 @@
+<?php
+
+class Database
+{
+    private $host = HOST;
+    private $db_name = DBNAME;
+    private $user = USER;
+    private $password = PASSWORD;
+    private $port = PORT;
+
+    private $db_connection;
+    private $statement;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name;
+        $option = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
+        try {
+            $this->db_connection = new PDO($dsn, $this->user, $this->password, $option);
+        } catch (PDOException) {
+            throw new LoggedException('Bad Gateway', 502);
+        }
+
+        try {
+            $this->db_connection->exec(Tables::USER_TABLE);
+            $this->db_connection->exec(Tables::OBJECT_TABLE);
+            $this->db_connection->exec(Tables::COMMENT_TABLE);
+            $this->db_connection->exec(Tables::LIKE_TABLE);
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+
+    public function query($query)
+    {
+        try {
+            $this->statement = $this->db_connection->prepare($query);
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+
+    public function bind($param, $value, $type = null)
+    {
+        try {
+            if (is_null($type)) {
+                if (is_int($value)) {
+                    $type = PDO::PARAM_INT;
+                } else if (is_bool($value)) {
+                    $type = PDO::PARAM_BOOL;
+                } else if (is_null($value)) {
+                    $type = PDO::PARAM_NULL;
+                } else {
+                    $type = PDO::PARAM_STR;
+                }
+            }
+            $this->statement->bindValue($param, $value, $type);
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+
+    public function execute()
+    {
+        try {
+            $this->statement->execute();
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+
+    public function fetch()
+    {
+        try {
+            $this->execute();
+            return $this->statement->fetch(PDO::FETCH_OBJ);
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+
+    public function fetchAll()
+    {
+        try {
+            $this->execute();
+            return $this->statement->fetchAll(PDO::FETCH_OBJ);
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+
+    public function rowCount()
+    {
+        try {
+            return $this->statement->rowCount();
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+    
+    public function lastInsertID()
+    {
+        try {
+            return $this->db_connection->lastInsertId();
+        } catch (PDOException) {
+            throw new LoggedException('Internal Server Error', 500);
+        }
+    }
+}
