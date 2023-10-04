e5952b077963e2b6652583b093ef3a493201cda9
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-04 13:48:43 +0700

    refactor: scripts for env config

--- scripts/dos/env-config.bat
+++ scripts/dos/env-config.bat
@@ -3,6 +3,9 @@ setlocal enabledelayedexpansion
 
 REM Prompt the user for a password
 set /p "PASSWORD=Enter a password for mysql environment: "
+echo.
+set /p "ADMIN_USERNAME=Enter the username for admin: "
+set /p "ADMIN_PASSWORD=Enter the password for admin: "
 
 REM Create a new .env file with the entered password
 (
@@ -13,6 +16,11 @@ REM Create a new .env file with the entered password
   echo MYSQL_HOST=db-moments
   echo MYSQL_DATABASE=moments
   echo MYSQL_PASSWORD=!PASSWORD!
+  echo.
+  echo # Environment variables for admin
+  echo ADMIN_USERNAME=!ADMIN_USERNAME!
+  echo ADMIN_PASSWORD=!ADMIN_PASSWORD!
+  echo ADMIN_EMAIL=!ADMIN_USERNAME!@gmail.com
 ) > .env
 
 echo.
