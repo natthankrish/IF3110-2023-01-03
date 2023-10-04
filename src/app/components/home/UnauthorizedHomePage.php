3046f7ace8499dca94a66b95f2d5c90b946a97af
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-04 13:50:40 +0700

    feat: added page for unauthorized user

---
+++ src/app/components/home/UnauthorizedHomePage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page</title>
+        <link rel="icon" href="<?= BASE_URL ?>/assets/icons/photos.png">
+        <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/home/HomeStyles.css">
+        <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/object/Navbar.css">
+    </head>
+    <body>
+    <?php include(dirname(__DIR__) . '/object/UnauthorizedNavbar.php') ?>
+        <div class="wrapper">
+            <div class="hello-text">   
+                <h1>View your moments in one view</h1>
+            </div>
+    </body>
+</html>
