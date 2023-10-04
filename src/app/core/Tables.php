d81caf1f592203a99e4e740b6d4c8bb756c2a6a2
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-04 13:52:49 +0700

    fix: query for tables

--- src/app/core/Tables.php
+++ src/app/core/Tables.php
@@ -3,49 +3,50 @@
 class Tables
 {
     public const USER_TABLE =
-    "CREATE TABLE IF NOT EXISTS User (
+    "CREATE TABLE IF NOT EXISTS user (
         user_id         INT                         AUTO_INCREMENT          PRIMARY KEY,
-        email           VARCHAR(26)                 UNIQUE,
-        password        VARCHAR(56),
-        name            VARCHAR(56),
-        user_role       ENUM('Admin', 'User'),
+        fullname        VARCHAR(256),
+        username        VARCHAR(256)                 UNIQUE,
+        email           VARCHAR(256)                 UNIQUE,
+        password        VARCHAR(256),
+        is_admin        BOOLEAN,
         storage         INT,
         storage_left    INT                  
     );";
 
     public const OBJECT_TABLE =
-    "CREATE TABLE Object (
+    "CREATE TABLE IF NOT EXISTS object (
         object_id       INT                         AUTO_INCREMENT          PRIMARY KEY,
         user_id         INT,
-        title           VARCHAR(112),
-        url             VARCHAR(255),
+        title           VARCHAR(64),
+        url             VARCHAR(256),
         isPublic        BOOLEAN,
         date            DATE,
-        location        VARCHAR(255),
-        description     VARCHAR(255),
+        location        VARCHAR(256),
+        description     VARCHAR(512),
         post_date       DATETIME,
 
-        FOREIGN KEY (user_id) REFERENCES User(user_id)
+        FOREIGN KEY (user_id) REFERENCES user(user_id)
     );";
 
     public const COMMENT_TABLE =
-    "CREATE TABLE Comment (
-        comment_id      INT                         AUTO_INCREMENT          PRIMARY_KEY,
+    "CREATE TABLE IF NOT EXISTS comment (
+        comment_id      INT                         AUTO_INCREMENT          PRIMARY KEY,
         object_id       INT,
         user_id         INT,
-        message         VARCHAR(255),
+        message         VARCHAR(512),
 
-        FOREIGN KEY (user_id) REFERENCES User(user_id),
-        FOREIGN KEY (object_id) REFERENCES Object(object_id),
+        FOREIGN KEY (user_id) REFERENCES user(user_id),
+        FOREIGN KEY (object_id) REFERENCES object(object_id)
     );";
 
     public const LIKE_TABLE =
-    "CREATE TABLE Like (
-        like_id         INT                         AUTO_INCREMENT          PRIMARY_KEY,
+    "CREATE TABLE IF NOT EXISTS likes (
+        like_id         INT                         AUTO_INCREMENT          PRIMARY KEY,
         object_id       INT,
         user_id         INT,
         
-        FOREIGN KEY (user_id) REFERENCES User(user_id),
-        FOREIGN KEY (object_id) REFERENCES Object(object_id),
+        FOREIGN KEY (user_id) REFERENCES user(user_id),
+        FOREIGN KEY (object_id) REFERENCES object(object_id)
     );";
 }
