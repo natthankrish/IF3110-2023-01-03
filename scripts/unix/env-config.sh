9368e9e114ee6ad0f80088afeab9211499f61c31
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2023-10-04 14:32:59 +0700

    refactor: unix script for env config

--- scripts/unix/env-config.sh
+++ scripts/unix/env-config.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Prompt the user for a password
-read -s -p "Enter a password for mysql environment: " PASSWORD
+read -p "Enter a password for mysql environment: " PASSWORD
+echo
+read -p "Enter the username for admin: " ADMIN_USERNAME
+read -p "Enter the password for admin: " ADMIN_PASSWORD
 
 # Create a new .env file with the entered password
-cat <<EOL > .env
+cat <<EOF > .env
 # Environment variables for mysql
 MYSQL_USER=moments
-MYSQL_ROOT_PASSWORD=$PASSWORD
+MYSQL_ROOT_PASSWORD=\$PASSWORD
 MYSQL_PORT=3306
 MYSQL_HOST=db-moments
 MYSQL_DATABASE=moments
-MYSQL_PASSWORD=$PASSWORD
-EOL
+MYSQL_PASSWORD=\$PASSWORD
 
-echo -e "\n.env file created successfully!"
+# Environment variables for admin
+ADMIN_USERNAME=\$ADMIN_USERNAME
+ADMIN_PASSWORD=\$ADMIN_PASSWORD
+ADMIN_EMAIL=\$ADMIN_USERNAME@gmail.com
+EOF
+
+echo
+echo ".env file created successfully!"
